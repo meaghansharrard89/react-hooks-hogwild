c0386dd45c690d5e48a7a13b81404675239c047c
Meaghan <meaghan.sharrard89@gmail.com>
2023-11-27 12:18:54 -0800

    updated code

--- src/components/App.js
+++ src/components/App.js
@@ -13,3 +13,16 @@ function App() {
 }
 
 export default App;
+
+/*1. send hogs data to component where it's looped through. top level of data lives here
+	-display a tile for each hog
+	-when the hog tile is clicked, display other details about the hog
+2. filter hogs by grease
+3. sort hogs based on name or weight
+
+APP
+	HOG INFO
+		DISPLAY HOGS
+		FILTER HOGS BY GREASE
+		SORT HOGS BY WEIGHT/NAME
+*/
